35b85cbb77c42f426a91b097757bfa38064f431c
RezaMohammadi <a.mohammadi@uva.nl>
2025-02-06 00:12:22 +0100

    new version

--- README.md
+++ README.md
@@ -1,4 +1,28 @@
 # Book-Data-Science
 
+First open "Book-Data-Science.Rproj" to start. 
+In the top-right tool-bar choose "Build" the "Build Book" with option "bookdown::bs4_book" to create HTML format. 
+
+useful links:
+
+https://bookdown.org/yihui/bookdown/
+
+citation: https://bookdown.org/yihui/bookdown/citations.html
+
+- in the file book.bib, add the citation key and the reference and use [@key], @key, or [a note, @key]
+- For cross references, use \@ref(label) for sub/section don't use e.g ":" in labels
+
+More info:
+
+- "_common.R" is a file that contains the common functions used in the book. 
 - Order of the chapters in the book are in "_bookdown.yml" file.
 - "_output.yml" e.g. primary color, font, size, etc. are in this file.
+
+ - - - 
+ 
+ For reference to examples use \@ref(exm:label)
+
+
+----
+
+To check the below link: https://github.com/daviddalpiaz/appliedstats/blob/master/README.md
--- _common.R
+++ _common.R
@@ -1,7 +1,34 @@
-set.seed( 42 )
+set.seed(42)
 
 # R options set globally
-# options( width = 60 )
+# options(width = 60)
+
+# Any package that is required by the script below is given here:
+inst_pkgs = load_pkgs =  c( "kableExtra", 
+                            "DMwR2", 
+                           "liver", 
+                           "ggplot2", 
+                           "plyr",    # for mutate function
+                           "dplyr",   # for filter and between functions
+                           "forcats", # for fct_collapse function
+                           "Hmisc",   # for handling missing values
+                           "naniar",  # for visualizing missing values
+                           
+                           "pROC", 
+                           "neuralnet",
+                           "psych", 
+                           "rpart", 
+                           "rpart.plot", 
+                           "C50", 
+                           "randomForest",
+                           "naivebayes",
+                           "factoextra")
+inst_pkgs = inst_pkgs[!(inst_pkgs %in% installed.packages()[,"Package"])]
+if (length(inst_pkgs)) install.packages(inst_pkgs)
+
+# Dynamically load packages
+pkgs_loaded = lapply(load_pkgs, require, character.only = TRUE)
+
 
 # Activate crayon output
 options(
@@ -12,26 +39,30 @@ options(
 
 # example chunk options set globally
 knitr::opts_chunk$set(
-    comment = "  ",
-    collapse = TRUE,
-    echo = TRUE, 
-    message = FALSE, 
-    warning = FALSE, 
-    error = FALSE, 
-    fig.align = 'center',
+    comment    = "  ",
+    collapse   = TRUE,
+    echo       = TRUE, 
+    message    = FALSE, 
+    warning    = FALSE, 
+    error      = FALSE,
+    fig.show   = 'hold',
+    fig.align  = 'center',
     fig.retina = 2,
-    fig.width = 6,
-    fig.asp = 2/3,
-    fig.show = "hold"
+    fig.width  = 6,
+    fig.asp    = 2/3
   )
 
-options( dplyr.print_min = 6, dplyr.print_max = 6 )
+options(dplyr.print_min = 6, dplyr.print_max = 6)
 
-ggplot2::theme_set( ggplot2::theme( 
-                            panel.background = ggplot2::element_rect( fill = "white", colour = "white", size = 0.5, linetype = "solid" ),
-                            panel.grid.major = ggplot2::element_line( size = 0.2, linetype = 'solid', colour = "gray77" ), 
-                            panel.grid.minor = ggplot2::element_line( size = 0.1, linetype = 'solid', colour = "gray90" ),
-                            axis.text  = ggplot2::element_text( size = 11 ), 
-                            axis.title = ggplot2::element_text( size = 12, face = "bold" ),
-                            title = ggplot2::element_text( size = 14, face = "bold" )
-                  ) )
+# for pdf output
+options(knitr.graphics.auto_pdf = TRUE)
+
+ggplot2::theme_set(ggplot2::theme( 
+                            panel.background = ggplot2::element_rect(fill = "white", colour = "white", linewidth = 0.5, linetype = "solid"),
+                            panel.grid.major = ggplot2::element_line(linewidth = 0.2, linetype = 'solid', colour = "gray77"), 
+                            panel.grid.minor = ggplot2::element_line(linewidth = 0.1, linetype = 'solid', colour = "gray90"),
+                            axis.text  = ggplot2::element_text(size = 11), 
+                            axis.title = ggplot2::element_text(size = 12, face = "bold"),
+                            title = ggplot2::element_text(size = 14, face = "bold"), 
+                            plot.title = ggplot2::element_text(hjust = 0.5)
+                  ))
